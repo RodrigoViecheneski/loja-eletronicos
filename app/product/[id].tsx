2d283ece98a51cfb3b7e09bb714b63dda91c19e8
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2024-10-15 22:09:27 -0300

    segundo commit

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,19 +1,30 @@
-import { Stack } from "expo-router";
-import { StyleSheet, SafeAreaView, ScrollView, View, Button } from "react-native";
-
+import { router, Stack, useLocalSearchParams } from "expo-router";
+import { Text, Image, StyleSheet, SafeAreaView, ScrollView, View, Button } from "react-native";
+import { getProductById } from "../../services/product";
 
 
 export default function Screen() {
+    const { id } = useLocalSearchParams();
+    const idProduct = parseInt(id as string);
+
+    const product = getProductById(idProduct);
+    if(!product) return router.back();
 
     const handleBuyButton = () => {
-        
+        alert("Você clicou no item: " + id);
     }
 
     return (
         <SafeAreaView style={styles.container}>
             <Stack.Screen options={{title: ''}}/>
             <ScrollView style={styles.area}>
-                
+                <Image 
+                    style={styles.img}
+                    source={{ uri: product.image}}
+                    resizeMode="cover"
+                />
+                <Text style={styles.title}>{product.title}</Text>
+                <Text style={styles.description}>{product.description}</Text>
             </ScrollView>
             <View style={styles.buttonArea}>
                 <Button
@@ -35,5 +46,21 @@ const styles = StyleSheet.create({
     },
     buttonArea: {
         padding: 10
+    },
+    img: {
+        width: '100%',
+        height: 250,
+        borderRadius: 10,
+        marginBottom: 20
+    },
+    title: {
+        fontSize: 27,
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    description: {
+        fontSize: 15,
+        color: '#555555',
+        marginBottom: 20
     }
 });
